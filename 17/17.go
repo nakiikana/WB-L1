0e1b42466cbebd6b4c35d32b659672038c1b0091
Seeds Odahviing <seedsoda@at-f6.msk.21-school.ru>
2024-06-25 15:06:56 +0300

    17 and 18

--- 17/17.go
+++ 17/17.go
@@ -2,15 +2,35 @@
 
 package main
 
-func BinarySort(arr []int, n int) int {
-	right := 0
-	left := len(arr) - 1
-	for right < left {
-		if arr[left/2] == n {
-			return left / 2
+import "fmt"
+
+// Бинарный поиск эффективен на отсортированном массиве чисел. В худшем случае сложность - O(log n),
+// в лучшем - O(1).
+func BinarySearch(arr []int, n int) int {
+	//Устанавливаем границы поиска
+	right := len(arr) - 1
+	left := 0
+	for left <= right {
+		//Находим значение, расположенное в середине массива.
+		middle := int((right + left) / 2)
+		//Если данное значение совпадает с искомым, то возвращаем его.
+		if arr[middle] == n {
+			return middle
 		}
-		if arr[left/2] > n {
-			right
+		//В противном случае сравнимаем с искомым: если значение по середине больше, то
+		//рассматриваем крайнюю левую половину массива, иначе - правую.
+		if arr[middle] > n {
+			right = middle - 1
+			continue
 		}
+		left = middle + 1
 	}
+	return -1
+}
+
+func main() {
+	arr := []int{-1, 6, 89, 123, 213, 1234, 2222}
+	fmt.Println("1234 -", BinarySearch(arr, 1234), "position")
+	fmt.Println("-1 -", BinarySearch(arr, -1), "position")
+	fmt.Println("777 -", BinarySearch(arr, 777), "position (not found)")
 }
